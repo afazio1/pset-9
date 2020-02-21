629a2863b95ae6b4f69ff699b27a461781a23604
Alexa <alexa.fazio04@gmail.com>
2020-02-21 17:31:22 -0500

    did things but will probably delete them later

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -9,11 +9,10 @@
 
 
 
-
-
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
- let canvas = document.getElementById('canvas');
- let ctx = canvas.getContext('2d');
+let canvas = document.getElementById('canvas');
+let playButton = document.getElementById('playButton');
+let ctx = canvas.getContext('2d');
 
 
 
@@ -22,19 +21,59 @@
 window.onload = init;
 
 
+// Listen for mouse moves
+// canvas.addEventListener('mousemove', function(event) {
+//   // Check whether point is inside circle
+//   if (ctx.isPointInPath(playButton, event.offsetX, event.offsetY)) {
+
+//   	//clear the canvas
+//   	ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   	//background effect for button
+//   	background = new Path2D();
+//     background.rect(canvas.width/2 - 100, canvas.height/2 - 50, 205, 105);
+//     ctx.fillStyle = 'black';
+//     ctx.fill(background);
+//     //actual button and play text
+//     ctx.fillStyle = 'lime';
+//     playButton.rect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
+//     ctx.fill(playButton);
+//     ctx.font = '40px PressStart2P';
+// 	ctx.fillStyle = 'black';
+// 	ctx.fillText('Play', canvas.width/2 - 77, canvas.height/2 + 20);
+
+    
+//   }
+//   else {
+//   	ctx.clearRect(0, 0, canvas.width, canvas.height);
+//   	//draw button without background shadow
+//   	ctx.fillStyle = 'lime';
+//     playButton.rect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
+//     ctx.fill(playButton);
+//     ctx.font = '40px PressStart2P';
+// 	ctx.fillStyle = 'black';
+// 	ctx.fillText('Play', canvas.width/2 - 77, canvas.height/2 + 20);
+//   }
+  
+  
+// });
+
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
 function init() {
-	ctx.fillStyle = 'lime';
-	ctx.strokeStyle = '3px black';
-	ctx.fillRect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
-	ctx.strokeRect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
-	ctx.font = '48px sans serif';
-	ctx.fillStyle = 'black';
-	ctx.fillText('Play', canvas.width/2 - 45, canvas.height/2 + 10);
+	
+	// playButton = new Path2D();
+	// playButton.rect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
+	// ctx.fillStyle = 'lime';
+	// ctx.fill(playButton);
+	// ctx.font = '40px PressStart2P';
+	// ctx.fillStyle = 'black';
+	// ctx.fillText('Play', canvas.width/2 - 77, canvas.height/2 + 20);
+
+	
 
 }
+
 
 
 
