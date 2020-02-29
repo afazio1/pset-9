8aeb9b5b4400056abc896deac42e59a0b9ec20d1
Alexa <alexa.fazio04@gmail.com>
2020-02-28 21:06:55 -0500

    deleted some comments

--- js/brickbreaker.js
+++ js/brickbreaker.js
@@ -3,8 +3,6 @@ const brickWidth = 90;
 const brickHeight = 50;
 
 
-
-
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
 let gameStarted;
 let go = 0;
@@ -23,44 +21,6 @@ let bricks = [];
 
 window.onload = startup;
 document.addEventListener('keydown', getArrows);
-
-
-// Listen for mouse moves
-// canvas.addEventListener('mousemove', function(event) {
-//   // Check whether point is inside circle
-//   if (ctx.isPointInPath(playButton, event.offsetX, event.offsetY)) {
-
-//   	//clear the canvas
-//   	ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   	//background effect for button
-//   	background = new Path2D();
-//     background.rect(canvas.width/2 - 100, canvas.height/2 - 50, 205, 105);
-//     ctx.fillStyle = 'black';
-//     ctx.fill(background);
-//     //actual button and play text
-//     ctx.fillStyle = 'lime';
-//     playButton.rect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
-//     ctx.fill(playButton);
-//     ctx.font = '40px PressStart2P';
-// 	ctx.fillStyle = 'black';
-// 	ctx.fillText('Play', canvas.width/2 - 77, canvas.height/2 + 20);
-
-    
-//   }
-//   else {
-//   	ctx.clearRect(0, 0, canvas.width, canvas.height);
-//   	//draw button without background shadow
-//   	ctx.fillStyle = 'lime';
-//     playButton.rect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
-//     ctx.fill(playButton);
-//     ctx.font = '40px PressStart2P';
-// 	ctx.fillStyle = 'black';
-// 	ctx.fillText('Play', canvas.width/2 - 77, canvas.height/2 + 20);
-//   }
-  
-  
-// });
-
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
@@ -108,7 +68,6 @@ function init() {
 			};
 			bricks.push(brick);
 		}
-	
 	}
 
 	if (go <= 1) {
@@ -126,14 +85,6 @@ function init() {
 	
 
  	playButton.onclick = game;
-
-	// playButton = new Path2D();
-	// playButton.rect(canvas.width/2 - 100, canvas.height/2 - 50, 200, 100);
-	// ctx.fillStyle = 'lime';
-	// ctx.fill(playButton);
-	// ctx.font = '40px PressStart2P';
-	// ctx.fillStyle = 'black';
-	// ctx.fillText('Play', canvas.width/2 - 77, canvas.height/2 + 20);
 
 }
 
@@ -217,10 +168,8 @@ function checkHit() {
 	for (let r = bricks.length - 1; r >= 0; r--) {
 		
 		if (ball.y - ball.radius <= bricks[r].y + brickHeight && bricks[r].hit === false && ball.x >= bricks[r].x && ball.x < bricks[r].x + brickWidth) {
-			console.log("gfhdjkgh");
 			ball.up = false;
 			bricks[r].hit = true;
-			console.log(bricks[r]);
 		}
 	}
 	//check if hits paddle
