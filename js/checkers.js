97b323b42412d62c45638631d592fd8908267616
Alexa <alexa.fazio04@gmail.com>
2020-03-02 15:55:05 -0500

    rats

--- js/checkers.js
+++ js/checkers.js
@@ -2,14 +2,26 @@
 
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
-
+let board = [];
 
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-
+let lightSquares = document.querySelectorAll('td');
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
-//window.onload = init;
+window.onload = init;
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
+function init() {
+	createArray();
+}
+
+function createArray() {
+	for (let i = 0; i < lightSquares.length; i++) {
+		if (i % 2 !== 0) {
+			board.push(lightSquares[i]);
+		}
+	}
+	console.log(lightSquares);
+}
