d3427b5bc7d95b398fe1a5c26ebc2c97756d5072
Alexa <alexa.fazio04@gmail.com>
2020-03-02 19:50:40 -0500

    remkaing things in checkersssss

--- js/checkers.js
+++ js/checkers.js
@@ -2,26 +2,82 @@
 
 
 ///////////////////// APP STATE (VARIABLES) /////////////////////////
-let board = [];
-
+let boardArray = [];
+let square;
 ///////////////////// CACHED ELEMENT REFERENCES /////////////////////
-let lightSquares = document.querySelectorAll('td');
+let graySquares = document.getElementsByClassName('gray');
+let table = document.getElementById("table");
 
 ///////////////////// EVENT LISTENERS ///////////////////////////////
 
 window.onload = init;
+table.onclick = selectedPiece;
+
+
 
 ///////////////////// FUNCTIONS /////////////////////////////////////
 
 function init() {
-	createArray();
+	createBoard();
+	createBluePiece();
 }
 
-function createArray() {
-	for (let i = 0; i < lightSquares.length; i++) {
-		if (i % 2 !== 0) {
-			board.push(lightSquares[i]);
-		}
+function createBoard() {
+	
+	for (let i = 0; i < graySquares.length - 1; i+=2) {
+		
+			boardArray.push(graySquares[i]);
+			boardArray.push(null);
+			boardArray.push(graySquares[i + 1]);
+			boardArray.push(null);
+		
 	}
-	console.log(lightSquares);
+	boardArray.pop();
+	console.log(boardArray);
+}
+function createBluePiece() {
+	let happened = false;
+	for (let b = 0; b < boardArray.length - 40; b+=2) {
+		if (b % 8 === 0 && b !== 0 && !happened) { //if there is a row, then increase b
+			b++;
+			happened = true;
+		}
+		else if (b === 17 && happened === true) { //if there is another row, the decrease b
+			b--;
+			happened = false;
+		}
+		let newBluePiece = {
+			index: b,
+			king: false
+		};
+		console.log(newBluePiece);
+	}
+}
+
+function selectedPiece(e) {
+	square = e.target;
+	console.log(square);
+	if (square.className === "piece-orange") {
+		square.className = "selected piece-orange";
+		table.onclick = moveOrangePiece;
+		console.log(square);
+	}
+	else if (square.className === "piece-blue") {
+		square.className = "selected piece-blue";
+		//.onclick = moveBluePiece;
+	}
+
+
+}
+function moveOrangePiece(e) {
+	//console.log(e.target.className);
+	console.log(e.target);
+	if (e.target.className === "" && e.target.className !== "piece-blue" && e.target.className !== "piece-orange") {
+		//console.log("chicken");
+		e.target.className = "piece-orange";
+	}
+	//console.log("rattatata");
+}
+function moveBluePiece(e) {
+
 }
